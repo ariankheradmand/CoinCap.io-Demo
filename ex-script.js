b9c4f2dd2207c66c54c88de319ff6343070099a3
Arian Kheradmand <ArianKheradmand@gmail.com>
2022-06-16 17:58:43 +0430

    div tabel

--- ex-script.js
+++ ex-script.js
@@ -1,48 +1,61 @@
 let listItems = document.body.querySelector(".tbody-class")
 var moneyFormatter  = new Intl.NumberFormat();
 function datalist (item) {
-  let Trow_tabel = document.createElement('tr'); // +
-  let rank = document.createElement('td'); // + 
-  let name = document.createElement('td'); // + 
-  let trd_pairs = document.createElement('td'); // +
-  trd_pairs.classList.add("column-center")
-  let top_pair = document.createElement('td');
-  let volumeUsd24Hr = document.createElement('td');
-  let total = document.createElement('td');
-  let status = document.createElement('td');
-  status.classList.add("column-center")
+  let Trow_tabel = document.createElement('div'); // +
+  Trow_tabel.classList.add("row-center","padding-t-b","tb-border",);
+
+  let rank = document.createElement('div'); // + 
+  
+  let name = document.createElement('div'); // + 
+  let trd_pairs = document.createElement('div'); // +
+  let top_pair = document.createElement('div');
+  let volumeUsd24Hr = document.createElement('div');
+  let total = document.createElement('div');
+  let status = document.createElement('div');
   let green = document.createElement("div")
   let red = document.createElement("div")
   let error = document.createElement("div")
+
+
+  
   let b = item.socket;
-  rank.textContent = item.rank ; // +
-  name.textContent = item.name + "\n" +"\n" +"\n" +"\n"; // +
+  rank.textContent = item.rank ;
+  rank.classList.add("padding","rank-size","text-align-center"); 
+
+  name.textContent = item.name ;
+  name.classList.add("padding","name-size");
+
+  status.classList.add("padding","other-size-ex")
   trd_pairs.textContent = moneyFormatter.format(Math.floor(item.tradingPairs))+ ("$"); // +
+  trd_pairs.classList.add("padding","other-size-ex");
   top_pair.textContent = item.exchangeId;
+  top_pair.classList.add("padding","other-size-ex");
   volumeUsd24Hr.textContent = moneyFormatter.format(Math.floor(item.volumeUsd))+ ("$");
+  volumeUsd24Hr.classList.add("padding","other-size-ex");
   total.textContent = moneyFormatter.format(Math.floor(item.percentTotalVolume))+ ("%");
+  total.classList.add("padding","other-size-ex");
   if (b === true) {
       status.appendChild(green);
-      green.classList.add("green")
+      green.classList.add("green","float")
   } if (b === false)  {
     status.appendChild(green);
-      green.classList.add("green")
+      green.classList.add("green","float")
   }if (b === null) {
     status.appendChild(red); 
-    red.classList.add("red")
+    red.classList.add("red","float")
   }
 
   
 
 
   listItems.appendChild(Trow_tabel);
-  listItems.appendChild(rank);
-  listItems.appendChild(name);
-  listItems.appendChild(trd_pairs);
-  listItems.appendChild(top_pair);
-  listItems.appendChild(volumeUsd24Hr);
-  listItems.appendChild(total);
-  listItems.appendChild(status);
+  Trow_tabel.appendChild(rank);
+  Trow_tabel.appendChild(name);
+  Trow_tabel.appendChild(trd_pairs);
+  Trow_tabel.appendChild(top_pair);
+  Trow_tabel.appendChild(volumeUsd24Hr);
+  Trow_tabel.appendChild(total);
+  Trow_tabel.appendChild(status);
 }
 
 fetch('https://api.coincap.io/v2/exchanges')
--- script.js
+++ script.js
@@ -7,28 +7,45 @@
 //   nav_1.classList.toggle("active")
 //   nav_2.classList.toggle("active")
 // })
-
-
 let listItems = document.body.querySelector(".tbody-class")
 var moneyFormatter  = new Intl.NumberFormat();
-function datalist (item) {
-  let Trow_tabel = document.createElement('tr');
-  let rank = document.createElement('td');
-  let cnt_name = document.createElement('td');
+ function datalist (item) {
+  let Trow_tabel = document.createElement('div');
+  Trow_tabel.classList.add("row-center","bd-size","tb-border",);
+
+  let rank = document.createElement('div');
+  rank.classList.add("padding","rank-size","text-align-center");
+
+
+
+  let cnt_name = document.createElement('div');
+  cnt_name.classList.add("padding","name-size");
   let link_name = document.createElement('a');
   link_name.classList.add("name-style");
   let name = document.createElement('div');
   let symbol = document.createElement('div');
   symbol.classList.add("symbol-name-table");
+
+
   let name_img = document.createElement('img');
-  let usdprice = document.createElement('td');
-  let marketcap = document.createElement('td');
-  let vwap24Hr = document.createElement('td');
-  vwap24Hr.classList.add("display-none");
-  let supply = document.createElement('td');
-  supply.classList.add("display-none");
-  let volumeUsd24Hr = document.createElement('td');
-  let changePercent24Hr = document.createElement('td');
+
+
+  let usdprice = document.createElement('div');
+  usdprice.classList.add("padding","other-size");
+
+
+  let marketcap = document.createElement('div');
+  marketcap.classList.add("padding","other-size");
+
+
+  let vwap24Hr = document.createElement('div');
+  vwap24Hr.classList.add("padding","other-size");
+  let supply = document.createElement('div');
+  supply.classList.add("padding","other-size");
+  let volumeUsd24Hr = document.createElement('div');
+  volumeUsd24Hr.classList.add("padding","other-size");
+  let changePercent24Hr = document.createElement('div');
+  changePercent24Hr.classList.add("padding","other-size");
 
   rank.textContent = item.rank ;
   name.textContent = item.name;
@@ -42,19 +59,20 @@ function datalist (item) {
 
 
   listItems.appendChild(Trow_tabel);
-  listItems.appendChild(rank);
-  listItems.appendChild(cnt_name);
+  Trow_tabel.appendChild(rank);
+  Trow_tabel.appendChild(cnt_name);
   cnt_name.appendChild(link_name);
   link_name.appendChild(name);
   link_name.appendChild(symbol);
 
-  listItems.appendChild(usdprice);
-  listItems.appendChild(marketcap);
-  listItems.appendChild(vwap24Hr);
-  listItems.appendChild(supply);
-  listItems.appendChild(volumeUsd24Hr);
-  listItems.appendChild(changePercent24Hr);
+  Trow_tabel.appendChild(usdprice);
+  Trow_tabel.appendChild(marketcap);
+  Trow_tabel.appendChild(vwap24Hr);
+  Trow_tabel.appendChild(supply);
+  Trow_tabel.appendChild(volumeUsd24Hr);
+  Trow_tabel.appendChild(changePercent24Hr);
 }
+
 
 fetch('https://api.coincap.io/v2/assets')
 .then (function (res) {
